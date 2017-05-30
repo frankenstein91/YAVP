3e5d605516f97f1518e993a745d7f7027231e6da
thymeore <mail.tomheyer@gmail.com>
2017-05-30 12:55:21 +0200

    Add Continents

--- YAVP/admin.py
+++ YAVP/admin.py
@@ -1,1 +1,10 @@
 # Register your models here.
+from django.contrib import admin
+
+from YAVP.models import Continent
+
+
+@admin.register(Continent)
+class ContinentAdmin(admin.ModelAdmin):
+    search_fields = ["Name"]
+    pass
--- YAVP/models.py
+++ YAVP/models.py
@@ -2,6 +2,13 @@
 from django.db import models
 
 
+class Continent(models.Model):
+    Name = models.CharField(blank=False, null=False, max_length=15)
+
+    def __str__(self):
+        return self.Name
+
+
 class Travel(models.Model):
     StartDate = models.DateField
     EndDate = models.DateField
